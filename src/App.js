40bf3b67cec690221abe022e1780111d77bc7c43
julietadirzieh <julidirzieh@gmail.com>
2021-08-02 15:33:13 -0300

    modifique los titulos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a Since 1981 - Fabricantes Textiles
         </p>
         <a
           className="App-link"
