865c390b9f72b97033de3bf9a5d73d241883c1c7
julietadirzieh <julidirzieh@gmail.com>
2021-08-09 11:32:14 -0300

    modifique el NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import "./styles.css"
+import "./NavBar.css";
 
-import "./components/NavBar";
 import NavBar from "./components/NavBar";
 
 class App extends Component {
