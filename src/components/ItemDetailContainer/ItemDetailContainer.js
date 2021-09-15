26bd4f2e42edd10339fd7d6ef6f13ed3da31f673
julietadirzieh <julidirzieh@gmail.com>
2021-09-15 15:23:44 -0300

    Desafío Clase 12

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -10,7 +10,7 @@ export const ItemDetailContainer = () => {
 
   console.log("itemdet", itemDetail);
 
-  const getItemDetail = async () => {
+  const getItemsDetail = async () => {
     const docs = [];
     const q = query(collection(db, "items"));
     const querySnapshot = await getDocs(q);
@@ -20,18 +20,103 @@ export const ItemDetailContainer = () => {
     setItemDetail(docs);
   };
 
+  const getItemDetail1 = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    const filteredItem = docs.filter((item) => {
+      return item.id === '2NqZeaI3OefMAztkPTU2';
+    });
+    setItemDetail(filteredItem);
+  };
+
+  const getItemDetail2 = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    const filteredItem = docs.filter((item) => {
+      return item.id === '95tF7f088h3phNCVC1E9';
+    });
+    setItemDetail(filteredItem);
+  };
+
+  const getItemDetail3 = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    const filteredItem = docs.filter((item) => {
+      return item.id === 'QhG2ZjfY5o3JOHu3ljQz';
+    });
+    setItemDetail(filteredItem);
+  };
+
+  const getItemDetail4 = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    const filteredItem = docs.filter((item) => {
+      return item.id === 'R3ZB3dCxF3M16fNqHwL9';
+    });
+    setItemDetail(filteredItem);
+  };
+
+  const getItemDetail5 = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    const filteredItem = docs.filter((item) => {
+      return item.id === 'gLjpXLLEH8bqcymXm1F8';
+    });
+    setItemDetail(filteredItem);
+  };
+
+  const getItemDetail6 = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    const filteredItem = docs.filter((item) => {
+      return item.id === 'inXtW1lZjaPhTsrWYcHV';
+    });
+    setItemDetail(filteredItem);
+  };
+
   useEffect(() => {
-    getItemDetail()
-    .finally(() => setLoading(false));
+    getItemsDetail()
+    getItemDetail1()
+      //getItemDetail2()
+      //getItemDetail3()
+      //getItemDetail4()
+      //getItemDetail5()
+      //getItemDetail6()
+      .finally(() => setLoading(false));
   }, []);
 
   if (loading) return <h1>Loading...</h1>;
 
   return (
     <div>
-      {itemDetail.map((item)=> {
-        return<ItemDetail item={item} key={item.id} />}) 
-      } 
+      {itemDetail.map((item) => {
+        return <ItemDetail item={item} key={item.id} />
+      })
+      }
     </div>
   )
 
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {ItemList} from '../ItemList/ItemList';
+import { ItemList } from '../ItemList/ItemList';
 import { db } from "../../firebase"
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 export const ItemListContainer = () => {
 
@@ -21,8 +21,30 @@ export const ItemListContainer = () => {
     getItems();
   }, []);
 
+  const getLisasCategory = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"), where("categoryId", "==", "lisas"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    setItems(docs);
+  };
+
+  const getEstampadasCategory = async () => {
+    const docs = [];
+    const q = query(collection(db, "items"), where("categoryId", "==", "estampadas"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    setItems(docs);
+  };
+
   return (
     <div>
+      <button onClick={getLisasCategory}>Telas lisas</button>
+      <button onClick={getEstampadasCategory}>Telas estampadas</button>
       <ItemList items={items} />
     </div>
   )
